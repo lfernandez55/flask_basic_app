8f049c60044aa75dfcbf14b69efbd15d8b0147dd
lfernandez55 <lfernandez@weber.edu>
2021-12-13 14:40:05 -0700

    Changed login, got rid of sql

--- app.py
+++ app.py
@@ -102,11 +102,11 @@ def create_app():
         db.session.commit()
 
     # Create 'admin@example.com' user with 'Admin' and 'Agent' roles
-    if not User.query.filter(User.email == 'luke.fernandez@gmail.com').first():
+    if not User.query.filter(User.email == 'admin@example.com').first():
         user = User(
-            email='luke.fernandez@gmail.com',
+            email='admin@example.com',
             email_confirmed_at=datetime.datetime.utcnow(),
-            password=user_manager.hash_password('Password2'),
+            password=user_manager.hash_password('Password1'),
         )
         user.roles.append(Role(name='Admin'))
         user.roles.append(Role(name='Agent'))
